2fa04ea801c10b96c55e312418991a2d26715472
woocash88 <79220274+woocash88@users.noreply.github.com>
2026-07-16 23:03:16 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,7 +2,7 @@ import urllib.request
 import json
 
 # Oficjalny endpoint Valve dla Europy
-VALVE_URL = "https://www.dota2.com/webapi/ILeaderboard/GetLeaderboard/v0001?division=europe"
+VALVE_URL = "https://www.dota2.com/webapi/ILeaderboard/GetDivisionLeaderboard/v0001?division=europe"
 
 def run_agent():
     print("Rozpoczynam pobieranie danych od Valve...")
